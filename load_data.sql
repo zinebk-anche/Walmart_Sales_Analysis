0623725553815b2bc7510f37c6e5ecb3432ea0db
zineb <zinebkanche@gmail.com>
2024-10-11 16:22:15 +0100

    Remove old SQL files from the root directory

--- load_data.sql
+++
@@ -1,9 +0,0 @@
--- Load data from the CSV file into the 'sales' table
--- Ensure that the file path is correct for your system
-LOAD DATA INFILE '/Users/zineb/Downloads/WalmartSalesData.csv (1).csv'
-INTO TABLE sales
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 ROWS
-(`invoice_id`, `branch`, `city`, `customer_type`, `gender`, `product_line`, `unit_price`, `quantity`, `tax_pct`, `total`, `date`, `time`, `payment`, `cogs`, `gross_margin_pct`, `gross_income`, `rating`);
